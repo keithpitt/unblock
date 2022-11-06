4a165bf64f80c84069dea048f23711e896158907
Keith Pitt <me@keithpitt.com>
2022-11-06 13:50:28 +0800

    Turn off strict mode which was causing double renders in dev mode

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // https://reactjs.org/blog/2022/03/29/react-v18.html#new-strict-mode-behaviors
+  // It was set to true, but it causes double renders in dev mode which is confusing and annoying
+  reactStrictMode: false,
 }
 
 module.exports = nextConfig
