eb3f52decd4270e1b3f680c41ed2d208115f70ac
Keith Pitt <me@keithpitt.com>
2022-11-07 21:39:00 +0800

    Played with making steps bigger when dragging around

--- components/Flow/StepNode.js
+++ components/Flow/StepNode.js
@@ -1,12 +1,13 @@
 import { memo } from 'react';
 import { Handle, Position } from 'reactflow';
+import classNames from 'classnames';
 
 import styles from './StepNode.module.css';
 
 const StepNode = ({ id, data, x, y, sourcePosition, targetPosition, selected }) => {
   return (
     <>
-      <div className={selected ? styles.selected : styles.default}>
+      <div className={classNames(styles.default, { [ styles.selected ]: selected })}>
         <div>{data.label}</div>
       </div>
 
