b1e26d289853730b973be5ca9777b01151fdb35b
Brett Jones <677025+BrettJay@users.noreply.github.com>
2022-11-16 10:32:20 +0800

    Update display of active step nodes
    * Update display of elapsed time on selected node, update design to
      match Figma
    * Add Buildkite logo to center of canvas
    * Themed colour for react flow edge paths

--- components/Flow/index.js
+++ components/Flow/index.js
@@ -28,7 +28,10 @@ const defaultEdgeOptions = {
   pathOptions: { borderRadius: 40 },
   // default: purple-600
   // selected: yellow-500
-  style: { stroke: "#4b19d5", strokeWidth: 2 },
+  style: {
+    stroke: "currentColor",
+    strokeWidth: 2,
+  },
 };
 
 const proOptions = {
@@ -42,6 +45,47 @@ const proOptions = {
 const fitViewOptions = {
   maxZoom: 1,
 };
+
+const Logo = () => (
+  <svg
+    width="28"
+    height="19"
+    viewBox="0 0 28 19"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    style={{
+      position: "absolute",
+      top: "24px",
+      left: "50%",
+      transform: "translateX(-50%);",
+      zIndex: 1000,
+    }}
+  >
+    <g clip-path="url(#clip0_76_31873)">
+      <path
+        d="M0 0V4.66667V9.33333L9.33333 14V9.33333V4.66667L0 0Z"
+        fill="#30F2A2"
+      />
+      <path
+        d="M18.6667 0V4.66667V9.33333L28.0001 4.66667L18.6667 0Z"
+        fill="#30F2A2"
+      />
+      <path
+        d="M18.667 9.33333V14V18.6667L28.0003 14V9.33333V4.66667L18.667 9.33333Z"
+        fill="#14CC80"
+      />
+      <path
+        d="M9.3335 4.66667V9.33333V14L18.6668 9.33333V4.66667V0L9.3335 4.66667Z"
+        fill="#14CC80"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_76_31873">
+        <rect width="28" height="18.6667" />
+      </clipPath>
+    </defs>
+  </svg>
+);
 
 function Flow({ roomId, initialNodes, initialEdges }) {
   const [nodes, setNodes] = useState(initialNodes);
@@ -59,6 +103,7 @@ function Flow({ roomId, initialNodes, initialEdges }) {
 
   return (
     <div className={styles.flow}>
+      <Logo />
       <ReactFlow
         defaultNodes={nodes}
         // onNodesChange={onNodesChange}
@@ -83,6 +128,7 @@ function Flow({ roomId, initialNodes, initialEdges }) {
         fitView
         maxZoom={1}
         fitViewOptions={fitViewOptions}
+        defaultZoom={1}
       >
         <Background
           variant="dots"
