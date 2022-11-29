c4795ca687a2da9614c7e2409db0dc05de5c0599
Keith Pitt <me@keithpitt.com>
2022-11-29 11:54:05 +0800

    Potential fix for reading files on prod

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   // https://reactjs.org/blog/2022/03/29/react-v18.html#new-strict-mode-behaviors
   // It was set to true, but it causes double renders in dev mode which is confusing and annoying
   reactStrictMode: false,
+
+  // https://robearlam.com/blog/nextjs-v11-enoent-no-such-file-or-directory-when-deployed-to-vercel
+  experimental: {
+    nftTracing: true
+  }
 }
 
 module.exports = nextConfig
